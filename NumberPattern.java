e0bf55f95f342c6cd2f80faabe1af7864e88416a
mscott717 <73617537+mscott717@users.noreply.github.com>
2021-10-06 19:26:21 -0600

    Add files via upload

---
+++ NumberPattern.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class NumberPattern {
+    // TODO: Write recursive printNumPattern() method
+
+    public static void printNumPattern(int startingNum, int changeNum){
+        System.out.print(startingNum + " ");
+        if(startingNum <= 0){
+            return;
+        }
+        printNumPattern(startingNum-changeNum,changeNum);
+        System.out.print(startingNum + " ");
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int num1;
+        int num2;
+
+        num1 = scnr.nextInt();
+        num2 = scnr.nextInt();
+        printNumPattern(num1, num2);
+
+    }
+}
